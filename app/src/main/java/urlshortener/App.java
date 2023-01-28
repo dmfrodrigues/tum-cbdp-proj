2bc47fbd18721ce23f7f8c379489e752ad613c43
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-01-28 14:49:39 +0100

    Added shutdown hook

--- app/src/main/java/urlshortener/App.java
+++ app/src/main/java/urlshortener/App.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
@@ -83,6 +85,12 @@ public class App {
 
         File file = new File("/tmp/registered");
         file.createNewFile();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> { try {
+            registry.unbind("raft");
+        } catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        } }));
 
         System.err.println("Raft registered to RMI registry");
     }
