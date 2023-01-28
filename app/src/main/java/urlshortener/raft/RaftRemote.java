131b6ebe531c749e189ba8343aa3249c85510fa4
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-01-28 21:27:32 +0100

    Documented RaftRemote methods

--- app/src/main/java/urlshortener/raft/RaftRemote.java
+++ app/src/main/java/urlshortener/raft/RaftRemote.java
@@ -11,12 +11,45 @@ public interface RaftRemote extends Remote {
     /**
      * RPC. New peer asks leader to join the network.
      * 
+     * The semantics of this function is: "the caller wants to be joined to the
+     * network that the callee belongs to".
+     * 
      * @throws RemoteException
      * @throws ServerNotActiveException
      */
     public void joinRPC() throws RemoteException, ServerNotActiveException;
 
+    /**
+     * RPC. Append entries.
+     * 
+     * The semantics of this function is: "the caller (the leader) informs the
+     * callee (a follower) that the log entries in entries are to be appended
+     * to the callee's log".
+     * 
+     * @param term          Term the leader is at
+     * @param prevLogIndex  Index of the log entry before the 1st item in entries
+     * @param prevLogTerm   Term of the log entry before the 1st item in entries
+     * @param entries       List of new entries to be appended to the callee log
+     * @param leaderCommit  Index of highest log entry known to be committed
+     * @return              Response; contains the term of the callee, and the
+     *                      result (success or otherwise)
+     * @throws RemoteException
+     */
     public RaftResponse<Boolean> appendEntriesRPC(int term, int prevLogIndex, int prevLogTerm, List<LogEntry> entries, int leaderCommit) throws RemoteException;
 
+    /**
+     * RPC. Request vote.
+     * 
+     * The semantics of this function is: "the caller (a candidate) requests the
+     * vote of the callee (a follower), so that it may become a leader".
+     * 
+     * @param term          Term the candidate is at
+     * @param lastLogIndex  Index of the last log entry of the candidate log
+     * @param lastLogTerm   Term of the last log entry of the candidate log
+     * @return              Response; contains the term of the callee, and the
+     *                      result (success or otherwise)
+     * @throws RemoteException
+     * @throws ServerNotActiveException
+     */
     public RaftResponse<Boolean> requestVoteRPC(int term, int lastLogIndex, int lastLogTerm) throws RemoteException, ServerNotActiveException;
 }
