7b28480fee1078de573ccc4ffd74744cf7442898
Diogo Rodrigues <dmfrodrigues2000@gmail.com>
2023-02-07 00:54:32 +0100

    Stuff

--- deploy_create.sh
+++ deploy_create.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
 
-if [ $# -eq 0 ] || [ $1 -lt 1 ]
+set -x
+
+if [ $# -eq 0 ] || [ $1 -lt 0 ]
 then
-  echo "Usage: ./deploy.sh num_peers(min 1 peer)"
+  echo "Usage: ./deploy.sh num_peers(min 0 peer)"
   exit 1
 fi
 
 export $(grep -v '^#' .env | xargs)
 
 echo Deploying leader
-az container create -g cbdp-resourcegroup --vnet cbdpVnet --subnet cbdpSubnet\
- --restart-policy Never --name leader\
- --image "$REGISTRY_NAME.azurecr.io"/peer\
- --registry-password=$REG_PWD --registry-username=$REG_USERNAME
+az storage share create --name cbdp-volume-leader \
+  --account-name $STORAGE_ACCOUNT \
+  --account-key $STORAGE_ACCOUNT_KEY
+az container create -g $RESOURCE_GROUP --vnet cbdpVnet --subnet cbdpSubnet\
+  --restart-policy Never --name leader\
+  --image "$REGISTRY_NAME.azurecr.io"/peer\
+  --azure-file-volume-account-name $STORAGE_ACCOUNT \
+  --azure-file-volume-account-key $STORAGE_ACCOUNT_KEY \
+  --azure-file-volume-share-name cbdp-volume-leader \
+  --azure-file-volume-mount-path /var/lib/postgresql/data \
+  --registry-password=$REG_PWD --registry-username=$REG_USERNAME
 
-LEADER=$(az container show --resource-group cbdp-resourcegroup --name leader --query ipAddress.ip --output tsv)
+LEADER=$(az container show --resource-group $RESOURCE_GROUP --name leader --query ipAddress.ip --output tsv)
 for (( i=1; i<=$1; i++ ))
 do
   echo Deploying peer $i to leader $LEADER
-  az container create -g cbdp-resourcegroup --vnet cbdpVnet --subnet cbdpSubnet\
+  az storage share create --name cbdp-volume-peer$i \
+    --account-name $STORAGE_ACCOUNT \
+    --account-key $STORAGE_ACCOUNT_KEY
+  az container create -g $RESOURCE_GROUP --vnet cbdpVnet --subnet cbdpSubnet\
     --restart-policy Never --name "peer$i"\
     --image "$REGISTRY_NAME.azurecr.io"/peer\
     --environment-variables LEADER_HOST="$LEADER"\
+    --azure-file-volume-account-name $STORAGE_ACCOUNT \
+    --azure-file-volume-account-key $STORAGE_ACCOUNT_KEY \
+    --azure-file-volume-share-name cbdp-volume-peer$i \
+    --azure-file-volume-mount-path /var/lib/postgresql/data \
     --registry-password=$REG_PWD --registry-username=$REG_USERNAME &
 done
 
--- init_db.sh
+++ init_db.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+if [ ! -f /var/lib/postgresql/data/pg_hba.conf ]
+then
+    ## Initialize DB
+    initdb -D /var/lib/postgresql/data
+    ## Allow external connections
+    echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
+    echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf
+fi
+
 ## Set user and password
 pg_ctl start -D /var/lib/postgresql/data
 psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'main'" | grep -q 1 || psql -U postgres -c "CREATE DATABASE main"
